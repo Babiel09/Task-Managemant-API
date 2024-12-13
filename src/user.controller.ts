eff8b03f9ac7edb2b80c65f4da9732db187b07fb
Gabriel Castro <estudosbabiel@gmail.com>
2024-12-13 16:47:42 -0300

    Primeiro GET com esse framework

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller("/user")
@@ -7,12 +7,49 @@ export class UserController{
     constructor(private readonly userService: UserService) {}
     
     @Post() 
-    async post(@Body() userData){
+    async post(@Body() userData:{name:string,email:string, password:string}){
         try{
-            await this.userService.Insert(userData);
+
+            const username:string = userData.name;
+            const userEmail:string = userData.email;
+            const userPass:string = userData.password;
+
+            if(!username){
+                throw new HttpException ("You need to pass the user NAME!", HttpStatus.BAD_REQUEST);
+            };
+
+            if(!userEmail){
+                throw new HttpException ("You need to pass the user EMAIL!", HttpStatus.BAD_REQUEST);
+            };
+
+            if(!userPass){
+                throw new HttpException ("You need to pass the user PASSWORD!", HttpStatus.BAD_REQUEST);
+            };
+            
+            const newUser = await this.userService.Insert(userData);
+            
+            if(!newUser){
+                throw new HttpException ("Unxpected error during the POST method! We can't show to you the userData!", HttpStatus.BAD_REQUEST);
+            };
             return {userData};
         } catch(err){
-            return {server:`Error during the POST method: ${err}`};
+            return {server:`${err}`};
         };
+    };
+
+    @Get()
+    async get(){
+        try{
+            const allUsers = await this.userService.SelectAll();
+            
+            if(!allUsers){
+                throw new HttpException ("A giant error happen, please check your internet connection and try again!", HttpStatus.INTERNAL_SERVER_ERROR)
+            };
+            
+            return {allUsers};
+
+        } catch(err){
+            return {server:`${err}`};
+        }
     };
 };
--- src/user.service.ts
+++ src/user.service.ts
@@ -1,17 +1,29 @@
 import { Body } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 
+const pr = new PrismaClient();
+const prisma = pr.user;
 export class UserService{
-    private pr = new PrismaClient();
-    private prisma = this.pr.user;
-
     async Insert(@Body() userData){
         try{
-            const postNewUser = await this.prisma.create;
+            const postNewUser = await prisma.create({
+                data:userData
+            });
             console.log("A requisição para dar o Insert dentro do DB deu certo!");
             return postNewUser;
         } catch(err){
             return {server:`Error during the Insert data to the DB! Error: ${err}`};
         };
     };
+
+    async SelectAll():Promise<User[]> {
+        try{
+
+            const mostraTudo = await prisma.findMany();
+            return mostraTudo;
+        } catch(err) {
+            console.error("Erro ao recuperar usuários:", err);
+            throw new Error(`Error during the SelectAll operation: ${err.message || err}`);
+        };
+    }
 };
